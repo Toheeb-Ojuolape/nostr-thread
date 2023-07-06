c74bcc3da8430b49311b724179595c28af42d6e3
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-07-06 14:30:32 +0100

    feat: added tags feature

--- src/components/NostrTags/NostrTags.tsx
+++ src/components/NostrTags/NostrTags.tsx
@@ -1,8 +1,40 @@
+import tags from "../../data/tags.json"
+import "./NostrTags.css"
 
+interface Tag{
+  name:string,
+  color:string,
+  id:number
+}
+
+interface TagProps{
+  tag:Tag
+}
+
+const Tag = ({tag}:TagProps) => {
+  return (
+    <div className="nostr-tag-item">
+    <div className="nostr-tag">
+      <span>Trending in Bitcoin</span>
+      <h3>{tag.name}</h3>
+      </div>
+    </div>
+
+  )
+}
 
 function NostrTags() {
   return (
-    <div></div>
+    <div className="nostr-tag-list">
+      <h3 style={{padding:"5px 15px"}}>Trends for You</h3>
+      <div className="grid">
+      {
+        tags.map((tag,i)=>(
+          <Tag tag={tag} key={i} />
+        ))
+      }
+      </div>
+    </div>
   )
 }
 
