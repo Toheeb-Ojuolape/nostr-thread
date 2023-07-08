c02123fab6aa3336177c993362272432e3e74a8a
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-07-08 20:13:55 +0100

    feat: added format handling for videos

--- src/components/NostrFeed/Content.tsx
+++ src/components/NostrFeed/Content.tsx
@@ -5,18 +5,25 @@ interface ContentProps {
 function Content({ content }: ContentProps) {
   const isImageLink = (text: string) => {
     // Regular expression to check if the text is a valid image link
-    const regex = /\.(gif|jpe?g|tiff?|png|webp|bmp|https?:\/\/tenor\.com\/.+\.gif)$/i;
+    const regex =
+      /\.(gif|jpe?g|tiff?|png|webp|bmp|https?:\/\/tenor\.com\/.+\.gif)$/i;
 
-    return regex.test(text)
+    return regex.test(text);
   };
 
-    
+  const isVideoLink = (text: string) => {
+    // Regular expression to check if the text is a valid image link
+    const regex = /\.(mov|mp4)$/i;
 
-  const isYoutubeLink = (text:string) =>{
+    return regex.test(text);
+  };
+
+  const isYoutubeLink = (text: string) => {
     // Regular expression to check if the text is a valid YouTube video link
-    const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})$/i;
-    return youtubeRegex.test(text)
-  }
+    const youtubeRegex =
+      /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})$/i;
+    return youtubeRegex.test(text);
+  };
 
   const splitContent = (content: string) => {
     // Split the content by whitespace to identify individual words/links
@@ -27,42 +34,41 @@ function Content({ content }: ContentProps) {
     const words = splitContent(content);
     const renderedContent: JSX.Element[] = [];
 
-    words.forEach((word:any, index) => {
-        if (isYoutubeLink(word)){
-          // If the word is a YouTube video link, embed it as an iframe
-          const videoId = word.match(/([a-zA-Z0-9_-]{11})$/i)[0];
-          const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    words.forEach((word: any, index) => {
+      if (isYoutubeLink(word)) {
+        // If the word is a YouTube video link, embed it as an iframe
+        const videoId = word.match(/([a-zA-Z0-9_-]{11})$/i)[0];
+        const embedUrl = `https://www.youtube.com/embed/${videoId}`;
 
-          renderedContent.push(
-            <div key={index} className="video-embed">
-              <iframe
-                width={"100%"}
-                src={embedUrl}
-                title="YouTube Video"
-                allowFullScreen
-                height={"300px"}
-              ></iframe>
-            </div>
-          );
-        } else if(isImageLink(word)) {
-          // If the word is an image link, wrap it in an <img> tag
-          renderedContent.push(
-            <img
-              onClick={() => window.open(word, "_blank")}
-              key={index}
-              src={word}
-              alt="Image"
-              style={{
-                cursor: "pointer",
-                width: "100%",
-                maxWidth:"500px",
-                objectFit: "cover",
-                margin: "5px 0px",
-              }}
-            />
-          );
-        }
-      else {
+        renderedContent.push(
+          <div key={index} className="video-embed">
+            <iframe
+              width={"100%"}
+              src={embedUrl}
+              title="YouTube Video"
+              allowFullScreen
+              height={"300px"}
+            ></iframe>
+          </div>
+        );
+      } else if (isVideoLink(word)) {
+        renderedContent.push(
+          <video controls className="mediaframe">
+            <source src={word} />
+          </video>
+        );
+      } else if (isImageLink(word)) {
+        // If the word is an image link, wrap it in an <img> tag
+        renderedContent.push(
+          <img
+            onClick={() => window.open(word, "_blank")}
+            key={index}
+            src={word}
+            alt="Image"
+            className="mediaframe"
+          />
+        );
+      } else {
         // Otherwise, render the word as plain text
         renderedContent.push(
           <span className="nostr-text" key={index}>
